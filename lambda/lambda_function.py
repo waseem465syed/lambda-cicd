14d1b39668d5a500e768554be06f2db1042b8e8b
waseem syed <waseem.syed465@gmail.com>
2025-06-30 11:01:53 +0100

    3rd

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello again and again from CICD Lambda again!')
+        'body': json.dumps('last hello from CICD Lambda again!')
         }
